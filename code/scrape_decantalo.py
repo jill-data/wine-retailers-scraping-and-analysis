ec39321e8ae455eae0b56970ec4f62748f13df65
jill-data <97563990+jill-data@users.noreply.github.com>
2022-11-02 13:50:03 +0000

    Review and data merge finished

--- code/scrape_decantalo.py
+++ code/scrape_decantalo.py
@@ -12,7 +12,7 @@ from selenium.webdriver.common.by import By
 from tqdm import tqdm
 
 listing_urls_path = Path('./data/url/decantalo_listings.json')
-export_path = './data/scrapped/decantalo_listings.csv'
+export_path = './data/scrapped/decantalo_listings_abv.csv'
 
 if not listing_urls_path.exists():
     driver = webdriver.Chrome()
@@ -88,7 +88,7 @@ def get_year(soup):
             class_="px-1 choose_comb selector-combinaciones SourceSansProRegular--mobile SourceSansProBold--desktop"
         ).text
         year = re.sub('\n', '', year)
-        return int(year)
+        return year
     except (AttributeError, ValueError):
         # Some listings don't specify the year
         return None
@@ -157,30 +157,34 @@ def get_reviews(driver, listing_url):
     return rating, num_reviews
 
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
 wine_info_all = []
-
-for wine_type, listing_url in tqdm(listing_urls):
+for wine_type, listing_url in tqdm(listing_urls[3225:]):
     listing_page = requests.get(listing_url)
     soup = BeautifulSoup(listing_page.content, 'html.parser')
 
     wine_info = {}
+    try:
+        wine_info['name'] = get_name(soup)
+    except:
+        # There are pages that are not wine listings, e.g:
+        # https://www.decantalo.com/uk/en/world-of-wine.html#/1-volume-75_cl/111-year-2018
+        continue
 
-    rating, num_reviews = get_reviews(driver, listing_url)
-    wine_info['rating'] = rating
-    wine_info['num_review'] = num_reviews
+    # rating, num_reviews = get_reviews(driver, listing_url)
+    # wine_info['rating'] = rating
+    # wine_info['num_review'] = num_reviews
 
     wine_info['wine_type'] = wine_type
-    wine_info['size (cL)'] = get_size(soup)
-    wine_info['name'] = get_name(soup)
-    wine_info['price'] = get_price(soup)
-    wine_info['country'] = get_country(soup)
-    wine_info['abv'] = get_abv(soup)
-    wine_info['year'] = get_year(soup)
+    # wine_info['size (cL)'] = get_size(soup)
+    # wine_info['price'] = get_price(soup)
+    # wine_info['country'] = get_country(soup)
+    # wine_info['abv'] = get_abv(soup)
+    # wine_info['year'] = get_year(soup)
     wine_info['url'] = listing_url
 
     wine_info_all.append(wine_info)
 
-wine_info_df = pd.DataFrame(wine_info_all)
+wine_info_df = pd.DataFrame(wine_info_all).to_clipboard()
 wine_info_df.to_csv(export_path, index=False)
